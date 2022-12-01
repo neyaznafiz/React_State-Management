19b27caff5745a42057e9a071e4c8e14d870f136
neyaznafiz <niiyazobug@gmail.com>
2022-12-01 21:15:00 +0600

    increment and decrement functionality impliment using useReducer in Counter component

--- src/pages/Counter.js
+++ src/pages/Counter.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 function Counter() {
+  const initialState = 0;
+  const reducer = (state, action) => {
+    if (action.type === "INCREMENT") {
+      return state + 1;
+    } else if (action.type === "DECREMENT") {
+      return state - 1;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="">
       <div className="flex flex-col gap-y-4 justify-center items-center h-40 w-72 rounded-lg text-2xl shadow-2xl">
-        <h1>0</h1>
+        <h1>{state}</h1>
 
         <div className="space-x-5">
-          <button className="text-lg bg-primary/40 px-2 py-1 rounded-full">
+          <button
+            onClick={() => dispatch({ type: "INCREMENT" })}
+            className="text-lg bg-primary/40 px-2 py-1 rounded-full"
+          >
             Increment
           </button>
 
-          <button className="text-lg bg-red-800/40 px-2 py-1 rounded-full">
+          <button
+            onClick={() => dispatch({ type: "DECREMENT" })}
+            className="text-lg bg-red-800/40 px-2 py-1 rounded-full"
+          >
             Decrement
           </button>
         </div>
